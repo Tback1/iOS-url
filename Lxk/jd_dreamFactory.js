c05fe588a45409d0984fbd3f41441a5d59c9906a
Thresh <thresh14@gmail.com>
2021-02-27 02:57:42 +0800

    update share

--- Lxk/jd_dreamFactory.js
+++ Lxk/jd_dreamFactory.js
@@ -37,12 +37,12 @@ const JD_API_HOST = 'https://m.jingxi.com';
 const notify = $.isNode() ? require('./sendNotify') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
 const randomCount = $.isNode() ? 20 : 5;
-let tuanActiveId = `6S9y4sJUfA2vPQP6TLdVIQ==`;
+let tuanActiveId = `TvjO5k4gaVqVHMRJIogd_g==`;
 const jxOpenUrl = `openjd://virtual?params=%7B%20%22category%22:%20%22jump%22,%20%22des%22:%20%22m%22,%20%22url%22:%20%22https://wqsd.jd.com/pingou/dream_factory/index.html%22%20%7D`;
 let cookiesArr = [], cookie = '', message = '';
 const inviteCodes = [
-  'hnC0M9W7FeUq0MHwwKBoQw==',
-  'FrXKuV3FkMkdVLL4TPii8Q==',
+  'hnC0M9W7FeUq0MHwwKBoQw==&FrXKuV3FkMkdVLL4TPii8Q==',
+  'FrXKuV3FkMkdVLL4TPii8Q==&FrXKuV3FkMkdVLL4TPii8Q==',
   // "",
 ];
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
